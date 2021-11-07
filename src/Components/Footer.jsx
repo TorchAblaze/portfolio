66000ae2b85fd82ac24257951f0db828ed974772
Tiffany Greathead <tiffanygreathead@gmail.com>
2021-11-07 07:45:12 -0800

    Add footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,19 @@
+function Footer() {
+  return (
+    <footer>
+      <p>
+        All photos including the background picture were taken by Tiffany
+        Greathead. See{" "}
+        <a
+          href="https://github.com/TorchAblaze/my-portfolio"
+          target="_blank"
+          rel="noreferrer"
+          >the code
+        </a>
+        {" "}for this webpage.
+      </p>
+    </footer>
+  )
+}
+
+export default Footer;
