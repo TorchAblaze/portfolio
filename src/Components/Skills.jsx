14574c286bad42e1f9592fe255800eac98f87b2a
Tiffany Greathead <tiffanygreathead@gmail.com>
2021-11-19 01:47:55 -0800

    Convert Info.jsx to Skills.jsx, remove projects and about sections

---
+++ src/Components/Skills.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function Skills() {
+  return (
+    <div className="box-styles">
+      <h2>Skills</h2>
+      <ul>
+        <div>
+          <li>JavaScript</li>
+          <li>Node.js</li>
+          <li>jQuery</li>
+          <li>React.js</li>
+        </div>
+        <div>
+          <li>Webpack</li>
+          <li>MySQL</li>
+          <li>C# / .NET</li>
+          <li>ASP.NET Core</li>
+        </div>
+        <div>
+          <li>GraphQL</li>
+          <li>Python</li>
+          <li>HTML / CSS</li>
+          <li>API Design</li>
+        </div>
+        <div>
+          <li>Git Version Control</li>
+          <li>Pair Programming</li>
+          <li>Functional Programming</li>
+          <li>Test Driven Development</li>
+        </div>
+      </ul>
+    </div>
+  )
+}
+
+export default Skills;
